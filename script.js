bfe85dad38d741945a940996e10fc81b9071b24f
pgLinhh <pghaothuat@gmail.com>
2023-07-25 13:10:49 +0700

    responsive mobile store

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const menuNav = document.querySelector('nav ul');
+const menuMobile = document.createElement('ul');
+
+menuToggle.addEventListener('click', () => {
+  menuNav.classList.toggle('menu-mobile');
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    menuNav.classList.remove('menu-mobile');
+  }
+});
+
+window.addEventListener('load', () => {
+  const menuItems = Array.from(menuNav.children);
+  menuItems.forEach(item => {
+    const menuItem = document.createElement('li');
+    menuItem.innerHTML = item.innerHTML;
+    menuMobile.appendChild(menuItem);
+  });
+});
+
+menuToggle.parentNode.insertBefore(menuMobile, menuToggle.nextSibling);
