cddd4e93448d4d9c9c082767159e01d1c0310018
pgLinhh <pghaothuat@gmail.com>
2023-07-26 15:46:12 +0700

    update 6

--- script2.js
+++ script2.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const clearCartButton = document.querySelector(".clear-cart");
     const cartItemsList = document.querySelector(".cart-items");
     const totalPriceElement = document.querySelector(".total-price");
+    const cartWindowContainer = document.querySelector(".cart-window-container");
   
     let totalPrice = 0;
     const cartItems = [];
@@ -13,15 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const productName = product.querySelector("h3").textContent;
         const productPrice = parseFloat(product.querySelector(".price").textContent);
   
-        // Check if the product is already in the cart
         const existingCartItem = cartItems.find((item) => item.name === productName);
   
         if (existingCartItem) {
-          // If the product is already in the cart, increase the quantity and update the total price
           existingCartItem.quantity++;
           existingCartItem.totalPrice = existingCartItem.quantity * productPrice;
         } else {
-          // If the product is not in the cart, add it
           cartItems.push({
             name: productName,
             price: productPrice,
@@ -30,22 +28,22 @@ document.addEventListener("DOMContentLoaded", () => {
           });
         }
   
-        // Sort cart items based on total price in descending order
         cartItems.sort((a, b) => b.totalPrice - a.totalPrice);
   
-        // Update the cart UI
         renderCartItems();
+        renderCartWindowItems();
       });
     });
   
     clearCartButton.addEventListener("click", () => {
-      cartItems.length = 0; // Clear the cart items array
+      cartItems.length = 0;
       totalPrice = 0;
-      renderCartItems(); // Update the cart UI
+      renderCartItems();
+      renderCartWindowItems();
     });
   
     function renderCartItems() {
-      cartItemsList.innerHTML = ""; // Clear the cart UI before rendering
+      cartItemsList.innerHTML = "";
   
       cartItems.forEach((item) => {
         const cartItem = document.createElement("li");
@@ -59,19 +57,17 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
         cartItemsList.appendChild(cartItem);
   
-        // Attach event listener to the remove button
         const removeButton = cartItem.querySelector(".remove-btn");
         removeButton.addEventListener("click", () => {
-          // Remove the item from the cart
           const itemIndex = cartItems.indexOf(item);
           if (itemIndex > -1) {
             cartItems.splice(itemIndex, 1);
             totalPrice -= item.totalPrice;
-            renderCartItems(); // Update the cart UI after removing
+            renderCartItems();
+            renderCartWindowItems();
           }
         });
   
-        // Attach event listener to the quantity buttons
         const quantityButtons = cartItem.querySelectorAll(".quantity-btn");
         quantityButtons.forEach((button) => {
           button.addEventListener("click", () => {
@@ -87,19 +83,68 @@ document.addEventListener("DOMContentLoaded", () => {
   
             cartItem.totalPrice = cartItem.quantity * cartItem.price;
   
-            // Sort cart items based on total price in descending order
             cartItems.sort((a, b) => b.totalPrice - a.totalPrice);
   
-            renderCartItems(); // Update the cart UI after changing the quantity
+            renderCartItems();
+            renderCartWindowItems();
           });
         });
       });
   
-      // Calculate the total price
       totalPrice = cartItems.reduce((total, item) => total + item.totalPrice, 0);
   
-      // Update the total price
       totalPriceElement.textContent = `Total: ${totalPrice} USD`;
     }
+  
+    function renderCartWindowItems() {
+      cartWindowContainer.innerHTML = "";
+  
+      if (cartItems.length === 0) {
+        const emptyMessage = document.createElement("p");
+        emptyMessage.textContent = "Giỏ hàng trống.";
+        cartWindowContainer.appendChild(emptyMessage);
+      } else {
+        cartItems.forEach((item) => {
+          const cartItem = document.createElement("div");
+          cartItem.className = "cart-window-item";
+          cartItem.innerHTML = `
+            <span>${item.name}</span>
+    
+            <span>Quantity: ${item.quantity}</span>
+            
+            <span>Price: ${item.price} USD</span>
+          `;
+          cartWindowContainer.appendChild(cartItem);
+        });
+      }
+  
+      const totalElement = document.createElement("p");
+      totalElement.className = "total-price-window";
+      totalElement.textContent = `Total: ${totalPrice} USD`;
+      cartWindowContainer.appendChild(totalElement);
+    }
+  
+    const cartWindow = document.querySelector(".cart-window-container");
+    document.querySelector(".fa-cart-shopping").addEventListener("click", () => {
+      cartWindow.classList.toggle("open");
+    });
   });
+  
+  document.addEventListener("click", (event) => {
+    const cartWindow = document.querySelector(".cart-window-container");
+    const cartIcon = document.querySelector(".fa-cart-shopping");
+  
+    if (!cartWindow.contains(event.target) && event.target !== cartIcon) {
+      cartWindow.classList.remove("open");
+    }
+  });
+  
+  function updateCartCounter() {
+    const cartCounter = document.getElementById("cart-counter");
+    const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+    cartCounter.textContent = totalQuantity;
+  }
+  
+  // Call the function to update the cart counter on page load
+  updateCartCounter();
   
